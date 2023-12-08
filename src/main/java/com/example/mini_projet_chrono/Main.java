39d6e6a7ad3858b47257a69676deef5a798f2809
Milton AMOUZOU <imtechbymilton@gmail.com>
2023-12-08 20:55:07 +0100

    Adding Main Class

--- src/main/java/com/example/mini_projet_chrono/Main.java
+++ src/main/java/com/example/mini_projet_chrono/Main.java
@@ -5,18 +5,18 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+
 import java.io.IOException;
 
-public class HelloApplication extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
+public class Main extends Application {
+
+    public void start( Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("View.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 458, 197);
         stage.setTitle("Chronomètre");
         stage.setScene(scene);
         stage.show();
     }
-
     public static void main(String[] args) {
         launch();
     }
