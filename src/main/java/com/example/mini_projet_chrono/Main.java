605d52225f1c6750aa9d75d536c24e17cfcd18c3
Milton AMOUZOU <imtechbymilton@gmail.com>
2023-12-08 21:03:42 +0100

    Rewrite Main Class

--- src/main/java/com/example/mini_projet_chrono/Main.java
+++ src/main/java/com/example/mini_projet_chrono/Main.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 public class Main extends Application {
 
     public void start( Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("View.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("View.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 458, 197);
         stage.setTitle("Chronomètre");
         stage.setScene(scene);
